a4ac1fc0bc2757bdb7471afa45896fd6d7554fd6
John Martin <jrmartin182@gmail.com>
2020-11-10 16:53:26 -0700

    initialization of backend

---
+++ dal/events.js
@@ -0,0 +1,4 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+const url = process.env.DB_API_URL
---
+++ routes/api/events.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const {
+
+} = require('../../DAL/events');
+
+/* GET home page. */
+router.get('/', async function(req, res) {
+
+});
+
+// POST
+router.post('/', async function(req, res){
+
+});
+
+// DELETE
+router.delete('/:id', async function(req, res){
+
+});
+
+//PUT
+router.put('/:id', async function(req, res){
+
+});
+
+//PATCH
+router.patch('/:id', async function(req, res){
+
+});
+
+module.exports = router;
